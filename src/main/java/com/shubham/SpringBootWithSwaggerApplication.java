19812d53fecaf656ea74bf2328ef7177f5c401e8
Shubham Yadav <46105856+yadav-shubh@users.noreply.github.com>
2024-01-07 16:53:57 +0530

    initial app

---
+++ src/main/java/com/shubham/SpringBootWithSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.shubham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithSwaggerApplication.class, args);
+	}
+
+}
